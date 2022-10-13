7a8473f83e788cc80786c7af609dae242d7c0b9c
Elijah Eccher <eeccher@cppwind.com>
2022-10-13 08:43:55 -0600

    fix init

--- disable_websearch.ps1
+++ disable_websearch.ps1
@@ -1,27 +1,27 @@
 function Set-WebSearch([Switch]$Enabled, [Switch]$Disabled) {
   $PropName = 'BingSearchEnabled'
   $RegKey   = 'Search'
-  $RegPath  = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\'
+  $RegPath  = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion'
   $State    = Check-Key -Path $RegPath -Key $RegKey -Property $PropName
 
   # Respond to Switch Parameters, else Toggle state
   if($Enabled){
-    Set-ItemProperty -Path $RegPath$RegKey -Name $PropName -Value 1
+    Set-ItemProperty -Path $RegPath\$RegKey -Name $PropName -Value 1
     "Web Search was enabled"
   }elseif($Disabled){
-    Set-ItemProperty -Path $RegPath$RegKey -Name $PropName -Value 0
+    Set-ItemProperty -Path $RegPath\$RegKey -Name $PropName -Value 0
     "Web Search was disabled"
   }else{
     Switch ($State) {
       "Enabled"  {
-        Set-ItemProperty -Path $RegPath$RegKey -Name $PropName -Value 0
+        Set-ItemProperty -Path $RegPath\$RegKey -Name $PropName -Value 0
         "Web Search was disabled"
       }
       "Disabled" {
-        Set-ItemProperty -Path $RegPath$RegKey -Name $PropName -Value 1
+        Set-ItemProperty -Path $RegPath\$RegKey -Name $PropName -Value 1
         "Web Search was enabled"
       }
-      default    { Get-ItemProperty -Path $RegPath$RegKey }
+      default    { Get-ItemProperty -Path $RegPath\$RegKey }
     }
   }
   "Restart Computer to apply changes"
@@ -31,10 +31,12 @@ function Check-Key($Key, $Path, $Property) {
   $ErrorActionCached = $ErrorActionPreference
   $ErrorActionPreference = 'Stop'
   try{
-    $PropValue = (Get-ItemProperty "$Path$Key"|findstr /i $Property).split(':')[1]
-  } catch [System.Management.Automation.SessionStateException]{
-    New-Item -Path $Path$Key
-    New-ItemProperty -Path "$Path$Key" -Name $Property -Value 1 -PropertyType "DWORD"
+    $PropValue = (Get-ItemProperty "$Path\$Key"|findstr /i $Property).split(':')[1]
+  } catch {
+    $ErrorActionPreference = 'SilentlyContinue'
+    New-Item -Path $Path\$Key
+    $ErrorActionPreference = 'Stop'
+    New-ItemProperty -Path "$Path\$Key" -Name $Property -Value 1 -PropertyType "DWORD"
     $PropValue = " 1"
   }
 
